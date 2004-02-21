dc147e534f76088d0630ba3f15e7d21a70dbecc5
Shiro Kawai <shiro@acm.org>
2004-02-21 04:15:31 +0000

    fixed kahua-error-string
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/Kahua/trunk@429 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- src/kahua/util.scm
+++ src/kahua/util.scm
@@ -5,7 +5,7 @@
 ;;  Copyright (c) 2004 Time Intermedia Corporation, All rights reserved.
 ;;  See COPYING for terms and conditions of using this software
 ;;
-;; $Id: util.scm,v 1.1 2004/01/22 13:32:12 shiro Exp $
+;; $Id: util.scm,v 1.2 2004/02/21 04:15:31 shiro Exp $
 
 ;; This module contains generally useful routines, which don't belong to
 ;; a particular module.
@@ -22,8 +22,8 @@
 
 (define (kahua-error-string e . maybe-detail?)
   (if (get-optional maybe-detail? #f)
-    (ref e 'message)
     (call-with-output-string
-      (cut with-error-to-port <> (cut report-error e)))))
+      (cut with-error-to-port <> (cut report-error e)))
+    (ref e 'message)))
 
 (provide "kahua/util")
