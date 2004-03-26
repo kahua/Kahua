775995dd3ebdce4e65946bc315b3b6159e33bf70
Nobuo Yamashita <nobsun@sampou.org>
2004-03-26 10:50:08 +0000

    Modified to make shell script for kahua-* commands
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/Kahua/trunk@582 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- src/fix-shebang.sh
+++ src/fix-shebang.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 GOSH="$1"
-SCRIPT="$2"
+LIB="$2"
+EXE="$3"
+SCRIPT="$4"
 sed "1s@#!/usr/bin/env gosh@#!$GOSH@" $SCRIPT.in > $SCRIPT.tmp && \
 mv $SCRIPT.tmp $SCRIPT && \
-chmod +x $SCRIPT
+chmod +x $SCRIPT && \
+echo '#!/bin/sh' > $SCRIPT.sh && \
+echo "$GOSH -I$LIB $EXE/$SCRIPT.scm" ' $@' >> $SCRIPT.sh && \
+chmod +x $SCRIPT.sh
+
 
