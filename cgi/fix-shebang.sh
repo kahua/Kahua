5ea2a21bc655076f8325b5d6707f836b8cfa7c6b
Nobuo Yamashita <nobsun@sampou.org>
2004-06-09 07:42:10 +0000

    modified log path where this kahua cgi script logs
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/Kahua/trunk@659 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- cgi/fix-shebang.sh
+++ cgi/fix-shebang.sh
@@ -2,13 +2,15 @@
 GOSH="$1"
 LIB="$2"
 SOCKBASE="$3"
-SCRIPT="$4"
+LOGPATH="$4"
+SCRIPT="$5"
 sed "1s@#!/usr/bin/env gosh@#!$GOSH@" $SCRIPT.in > $SCRIPT.tmp && \
 mv $SCRIPT.tmp $SCRIPT.pre0 && \
 sed "s@##libdir##@$LIB@" $SCRIPT.pre0 > $SCRIPT.pre1 && \
-sed "s@##KAHUA_SOCKET_BASE##@$SOCKBASE@" $SCRIPT.pre1 > $SCRIPT.tmp
+sed "s@##KAHUA_SOCKET_BASE##@$SOCKBASE@" $SCRIPT.pre1 > $SCRIPT.pre2 && \
+sed "s@##KAHUA_CGI_LOGPATH##@$LOGPATH@" $SCRIPT.pre2 > $SCRIPT.tmp && \
 mv $SCRIPT.tmp $SCRIPT && \
-rm -f $SCRIPT.pre0 $SCRIPT.pre1 && \
+rm -f $SCRIPT.pre0 $SCRIPT.pre1 $SCRIPT.pre2 && \
 chmod +x $SCRIPT
 
 
