9c12db3696d2f4322c15f36bd71fe25b9db3524a
Kimura Fuyuki <fuyuki@hadaly.org>
2004-06-28 09:58:18 +0000

    Build process cleanups
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/Kahua/trunk@673 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- cgi/fix-shebang.sh
+++ cgi/fix-shebang.sh
@@ -9,7 +9,7 @@ mv $SCRIPT.tmp $SCRIPT.pre0 && \
 sed "s@##libdir##@$LIB@" $SCRIPT.pre0 > $SCRIPT.pre1 && \
 sed "s@##KAHUA_SOCKET_BASE##@$SOCKBASE@" $SCRIPT.pre1 > $SCRIPT.pre2 && \
 sed "s@##KAHUA_CGI_LOGPATH##@$LOGPATH@" $SCRIPT.pre2 > $SCRIPT.tmp && \
-mv $SCRIPT.tmp $SCRIPT && \
+mv -f $SCRIPT.tmp $SCRIPT && \
 rm -f $SCRIPT.pre0 $SCRIPT.pre1 $SCRIPT.pre2 && \
 chmod +x $SCRIPT
 
