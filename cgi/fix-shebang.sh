5e01b5eaf1582e417dfc16d6852e26a23d466749
Hisazumi Kenji <nel@soraneko.com>
2005-08-06 05:10:50 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/Kahua/trunk@843 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- cgi/fix-shebang.sh
+++ cgi/fix-shebang.sh
@@ -4,9 +4,10 @@ LIB="$2"
 SOCKBASE="$3"
 LOGPATH="$4"
 FASTCGIP="$5"
-SCRIPT="$6"
+SCRIPT_IN="$6"
+SCRIPT_OUT="$7"
 
-sed "1s@#!/usr/bin/env gosh@#!$GOSH@" $SCRIPT.in > $SCRIPT.tmp && \
+sed "1s@#!/usr/bin/env gosh@#!$GOSH@" $SCRIPT_IN > $SCRIPT.tmp && \
 mv $SCRIPT.tmp $SCRIPT.pre0 && \
 sed "s@##libdir##@$LIB@" $SCRIPT.pre0 > $SCRIPT.pre1 && \
 sed "s@##KAHUA_SOCKET_BASE##@$SOCKBASE@" $SCRIPT.pre1 > $SCRIPT.pre2 && \
@@ -25,11 +26,10 @@ case $FASTCGIP in
 	;;
 esac && \
 
-mv -f $SCRIPT.tmp $SCRIPT && \
-chmod +x $SCRIPT && \
+mv -f $SCRIPT.tmp $SCRIPT_OUT && \
+chmod +x $SCRIPT_OUT && \
 
-rm -f $SCRIPT.pre0 $SCRIPT.pre1 $SCRIPT.pre2 $SCRIPT.pre3 $SCRIPT.pre4 $SCRIPT.pre5 && \
-chmod +x $SCRIPT
+rm -f $SCRIPT.pre0 $SCRIPT.pre1 $SCRIPT.pre2 $SCRIPT.pre3 $SCRIPT.pre4 $SCRIPT.pre5
 
 
 
