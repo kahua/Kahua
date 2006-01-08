75e69f201e5bc7850e313b1c091ffb709a66d53c
SHIBATA Tomohisa <shibata@kahua.org>
2006-01-08 12:34:43 +0000

    Added <kahua-user> subclass test.
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/Kahua/trunk@1403 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- test/user.scm
+++ test/user.scm
@@ -1,5 +1,5 @@
 ;; -*- coding: euc-jp ; mode: scheme -*-
-;; $Id: user.scm,v 1.4 2005/12/13 17:33:35 cut-sea Exp $
+;; $Id: user.scm,v 1.5 2006/01/08 12:34:43 shibata Exp $
 
 ;; test kahua.user
 
@@ -67,82 +67,75 @@
 		      (kahua-user-password-change user "manapua" "manahune")
 		      (and-let* ((u (kahua-check-user "shiro" "manahune")))
 				(ref u 'login-name)))))
-		      
+
 (test* "kahua-user-password-change-force" "shiro"
        (with-db (db *dbname*)
 		(let1 user (find-kahua-instance <kahua-user> "shiro")
 		      (kahua-user-password-change-force user "urashima")
 		      (and-let* ((u (kahua-check-user "shiro" "urashima")))
 				(ref u 'login-name)))))
-		      
+
 ;;
 ;; app subclass of <kahua-user>.
 ;;
-(define-class <test-user> (<kahua-user>)
-  ((fname  :allocation :persistent :init-keyword :fname
-	   :init-value "" :accessor fname-of)
-   (lname  :allocation :persistent :init-keyword :lname
-	   :init-value "" :accessor lname-of)))
+(define-class <my-kahua-user> (<kahua-user>) ())
 
-
-(test* "check-user (in the empty db)" #f
+(test* "kahua-check-user (in the empty db)" #f
        (with-db (db *dbname*)
-         (check-user <test-user> "cut-sea" "cutsea")))
+         (kahua-check-user "shiro" "shiro")))
 
-(test* "add-user" #t
+(test* "kahua-add-user" #t
        (with-db (db *dbname*)
-         (every (cut is-a? <> <test-user>)
-                (list (add-user <test-user> "shiro"  "manapua")
-                      (add-user <test-user> "nobsun" "punahou")
-                      (add-user <test-user> "cut-sea"  "kamosimakuri-bu")))))
+         (every (cut is-a? <> <my-kahua-user>)
+                (list (kahua-add-user "shiro"  "manapua")
+                      (kahua-add-user "nobsun" "punahou")
+                      (kahua-add-user "admin"  "kamehameha")))))
 
-(test* "find-user" "cut-sea"
+(test* "kahua-find-user" "shiro"
        (with-db (db *dbname*)
-         (and-let* ((u (find-user <test-user> "cut-sea")))
+         (and-let* ((u (kahua-find-user "shiro")))
            (ref u 'login-name))))
 
-(test* "find-user" #f
+(test* "kahua-find-user" #f
        (with-db (db *dbname*)
-         (and-let* ((u (find-user <test-user> "shirok")))
+         (and-let* ((u (kahua-find-user "shirok")))
            (ref u 'login-name))))
 
-(test* "add-user (dup)" #f
+(test* "kahua-add-user (dup)" #f
        (with-db (db *dbname*)
-         (add-user <test-user> "nobsun" "makapuu")))
+         (kahua-add-user "nobsun" "makapuu")))
 
-(test* "add-user (non-dup)" #t
+(test* "kahua-add-user (non-dup)" #t
        (with-db (db *dbname*)
-         (not (not (add-user <test-user> "guest" "molokai")))))
+         (not (not (kahua-add-user "guest" "molokai")))))
          
-(test* "check-user" "cut-sea"
+(test* "kahua-check-user" "shiro"
        (with-db (db *dbname*)
-         (and-let* ((u (check-user <test-user> "cut-sea" "kamosimakuri-bu")))
+         (and-let* ((u (kahua-check-user "shiro" "manapua")))
            (ref u 'login-name))))
 
-(test* "check-user" #f
+(test* "kahua-check-user" #f
        (with-db (db *dbname*)
-         (and-let* ((u (check-user <test-user> "shiro" "makapuu")))
+         (and-let* ((u (kahua-check-user "shiro" "makapuu")))
            (ref u 'login-name))))
 
-(test* "check-user" #f
+(test* "kahua-check-user" #f
        (with-db (db *dbname*)
-         (and-let* ((u (check-user <test-user> "shirok" "makapuu")))
+         (and-let* ((u (kahua-check-user "shirok" "makapuu")))
            (ref u 'login-name))))
 
-(test* "user-password-change" "cut-sea"
+(test* "kahua-user-password-change" "shiro"
        (with-db (db *dbname*)
-		(let1 user (find-kahua-instance <test-user> "cut-sea")
-		      (user-password-change user "kamosimakuri-bu" "kamosiJapan")
-		      (and-let* ((u (check-user <test-user> "cut-sea" "kamosiJapan")))
+		(let1 user (kahua-find-user "shiro")
+		      (kahua-user-password-change user "manapua" "manahune")
+		      (and-let* ((u (kahua-check-user "shiro" "manahune")))
 				(ref u 'login-name)))))
-		      
-(test* "user-password-change-force" "cut-sea"
+
+(test* "kahua-user-password-change-force" "shiro"
        (with-db (db *dbname*)
-		(let1 user (find-kahua-instance <test-user> "cut-sea")
-		      (user-password-change-force user "kamosuzo!")
-		      (and-let* ((u (check-user <test-user> "cut-sea" "kamosuzo!")))
+		(let1 user (kahua-find-user "shiro")
+		      (kahua-user-password-change-force user "urashima")
+		      (and-let* ((u (kahua-check-user "shiro" "urashima")))
 				(ref u 'login-name)))))
-		      
-		      
 
 (test-end)
