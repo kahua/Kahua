b1c82d28d93e1ded3a3fc5fdb1999565fa8836f3
SHIBATA Tomohisa <shibata@kahua.org>
2006-01-08 12:33:20 +0000

    Removed add-user check-user find-user user-password-change \         user-password-change-force user-has-role?. Added <kahua-user-meta>. Changed <kahua-user> to use <kahua-user-meta> meta class.
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/Kahua/trunk@1402 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- src/kahua/user.scm
+++ src/kahua/user.scm
@@ -3,7 +3,7 @@
 ;;  Copyright (c) 2003 Time Intermedia Corporation, All rights reserved.
 ;;  See COPYING for terms and conditions of using this software
 ;;
-;; $Id: user.scm,v 1.4 2005/12/13 16:40:00 cut-sea Exp $
+;; $Id: user.scm,v 1.5 2006/01/08 12:33:20 shibata Exp $
 
 (define-module kahua.user
   (use kahua.persistence)
@@ -15,13 +15,19 @@
   (export <kahua-user> kahua-add-user kahua-check-user kahua-find-user
           kahua-user-password-change kahua-user-password-change-force
 	  kahua-user-has-role?
-	  add-user check-user find-user
-	  user-password-change user-password-change-force
-	  user-has-role?
 	  ))
 (select-module kahua.user)
 
-(define-class <kahua-user> (<kahua-persistent-base>)
+(define-class <kahua-user-meta> (<class>)
+  ((%user-class :allocation :class)))
+
+(define-class <kaua-user-mixin> () () :metaclass <kahua-user-meta>)
+
+(define-method initialize ((class <kahua-user-meta>) initargs)
+  (next-method)
+  (slot-set! class '%user-class class))
+
+(define-class <kahua-user> (<kahua-persistent-base> <kaua-user-mixin>)
   ((login-name    :allocation :persistent
                   :init-keyword :login-name :init-value #f)
    (password-hash :allocation :persistent
@@ -35,82 +41,50 @@
 (define-method key-of ((self <kahua-user>))
   (ref self 'login-name))
 
-(define (kahua-find-user login-name)
-  (find-kahua-instance <kahua-user> login-name))
+(define (kahua-current-user-class)
+  (ref <kahua-user> '%user-class))
 
-;; for user defined kahua-user subclass
-(define-method find-user ((cls <kahua-persistent-meta>) login-name)
-  (find-kahua-instance cls login-name))
+(define (kahua-find-user login-name)
+  (find-kahua-instance (kahua-current-user-class) login-name))
 
 (define (kahua-add-user login-name password)
   (if (kahua-find-user login-name)
     #f
-    (make <kahua-user>
+    (make (kahua-current-user-class)
       :login-name login-name
       :password-hash (crypt-passwd password))))
-
-;; for user defined kahua-user subclass
-(define-method add-user ((cls <kahua-persistent-meta>) login-name password)
-  (if (find-user cls login-name)
-      #f
-      (make cls
-	:login-name login-name
-	:password-hash (crypt-passwd password))))
 
 (define (kahua-check-user login-name password)
   (find (lambda (user)
           (and (equal? (ref user 'login-name) login-name)
                (not (ref user 'inactive))
                (match-passwd password (ref user 'password-hash))))
-        (make-kahua-collection <kahua-user>)))
-
-;; for user defined kahua-user subclass
-(define-method check-user ((cls <kahua-persistent-meta>) login-name password)
-  (find (lambda (user)
-	  (and (equal? (ref user 'login-name) login-name)
-	       (not (ref user 'inactive))
-	       (match-passwd password (ref user 'password-hash))))
-	(make-kahua-collection cls)))
+        (make-kahua-collection (kahua-current-user-class))))
 
 ;; user must be <kahua-user>.  caller must sync db.
 (define (kahua-user-password-change user old-password new-password)
-  (and (is-a? user <kahua-user>)
+  (and (is-a? user (kahua-current-user-class))
        (kahua-check-user (ref user 'login-name) old-password)
        (kahua-user-password-change-force user new-password)
        #t))
 
-;; for user defined kahua-user subclass
-(define-method user-password-change ((usr <kahua-user>) old-password new-password)
-  (and (check-user (class-of usr) (ref usr 'login-name) old-password)
-       (user-password-change-force usr new-password)
-       #t))
-
 ;; user must be <kahua-user>.  caller must sync db.
 (define (kahua-user-password-change-force user new-password)
-  (and (is-a? user <kahua-user>)
+  (and (is-a? user (kahua-current-user-class))
        (set! (ref user 'password-hash) (crypt-passwd new-password))
        #t))
 
-;; for user defined kahua-user subclass
-(define-method user-password-change-force ((usr <kahua-user>) new-password)
-  (and (set! (ref usr 'password-hash) (crypt-passwd new-password))
-       #t))
 
 ;; user may be <kahua-user> object or #f
 ;; role-alist's spec isn't fixed yet.  for now, we assume it's a list of
 ;; symbols.
 ;; ROLES arg is also a list of symbols.  Later we might add move syntax.
 (define (kahua-user-has-role? user roles)
-  (and (is-a? user <kahua-user>)
+  (and (is-a? user (kahua-current-user-class))
        (list? roles)
        (find (lambda (urole) (find (cut eq? <> urole) roles))
              (ref user 'role-alist))))
 
-;; for user defined kahua-user subclass
-(define-method user-has-role? ((usr <kahua-user>) roles)
-  (and (list? roles)
-       (find (lambda (urole) (find (cut eq? <> urole) roles))
-	     (ref usr 'role-alist))))
 
 ;; internal utility
 (define (crypt-passwd passwd)
