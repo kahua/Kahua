b35d59f9339e5cf480f2085891b0e0c70ea5e8b9
Tatsuya BIZENN <bizenn@gmail.com>
2007-04-26 03:56:21 +0000

    Fix wrong format of entity reference node (&, &:, &/).
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/Kahua/trunk@2663 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- src/kahua/server.scm
+++ src/kahua/server.scm
@@ -4,7 +4,7 @@
 ;;  Copyright (c) 2003-2004 Time Intermedia Corporation, All rights reserved.
 ;;  See COPYING for terms and conditions of using this software
 ;;
-;; $Id: server.scm,v 1.106 2007/04/26 01:56:35 bizenn Exp $
+;; $Id: server.scm,v 1.107 2007/04/26 03:56:21 bizenn Exp $
 
 ;; This module integrates various kahua.* components, and provides
 ;; application servers a common utility to communicate kahua-server
@@ -1452,7 +1452,10 @@
 (define-element & (attrs auxs contents context cont)
   (cont (list (apply make-no-escape-text-element
 		     (map (lambda (c)
-			    #`"&,|c|;")
+			    (cond ((or (string? c) (symbol? c)) (format "&~a;" c))
+				  ((char?   c) (format "&#x~x;" (char->ucs c)))
+				  ((integer? c) (format "&#x~x;" c))
+				  (else (error "& node require string or symbol(character name), integer(character code) or character itself, but got " c))))
 			  contents)))
 	context))
 
