abf0ff969cd0e2b0c1cca3e1b38f6b3b6d0845c4
Tatsuya BIZENN <bizenn@gmail.com>
2007-06-20 06:35:54 +0000

    Modernized.
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/Kahua/trunk@2852 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- src/kahua/dbm.scm
+++ src/kahua/dbm.scm
@@ -1,15 +1,14 @@
 ;; Provides dbm interface on top of Kahua db
 ;;
-;;  Copyright (c) 2003 Scheme Arts, L.L.C., All rights reserved.
-;;  Copyright (c) 2003 Time Intermedia Corporation, All rights reserved.
+;;  Copyright (c) 2003-2007 Scheme Arts, L.L.C., All rights reserved.
+;;  Copyright (c) 2003-2007 Time Intermedia Corporation, All rights reserved.
 ;;  See COPYING for terms and conditions of using this software
 ;;
-;; $Id: dbm.scm,v 1.1 2004/01/14 00:50:54 shiro Exp $
+;; $Id: dbm.scm,v 1.2 2007/06/20 06:35:54 bizenn Exp $
 
 ;; This module implements dbm protocol on top of kahua's persistent objects.
 
 (define-module kahua.dbm
-  (use srfi-2)
   (use kahua.persistence)
   (use gauche.sequence)
   (extend dbm)
@@ -24,13 +23,9 @@
   ())
 
 (define-class <kahua-dbm-record> (<kahua-persistent-base>)
-  ((key      :allocation :persistent :init-value "" :init-keyword :key)
-   (value    :allocation :persistent :init-value "" :init-keyword :value)
-   (deleted? :allocation :persistent :init-value #f))
+  ((key      :allocation :persistent :init-value "" :init-keyword :key :index :unique)
+   (value    :allocation :persistent :init-value "" :init-keyword :value))
   :metaclass <kahua-dbm-record-meta>)
-
-(define-method key-of ((self <kahua-dbm-record>))
-  (ref self 'key))
 
 ;; dbm protocol implementation
 ;; :path argument takes the _class_ object
@@ -63,46 +58,42 @@
   (let* ((k (%dbm-k2s self key))
          (v (%dbm-v2s self value))
          (class (kahua-dbm-class self))
-         (obj (find-kahua-instance class k)))
+         (obj (find-kahua-instance class 'key k)))
     (if obj
-      (begin
         (set! (ref obj 'value) v)
-        (set! (ref obj 'deleted?) #f))
-      (make class :key k :value v))
+	(make class :key k :value v))
     #t))
 
 (define-method dbm-get ((self <kahua-dbm>) key . args)
   (next-method)
   (let* ((k (%dbm-k2s self key))
-         (class (kahua-dbm-class self))
-         (obj (find-kahua-instance class k)))
-    (if (and obj (not (ref obj 'deleted?)))
-      (%dbm-s2v self (ref obj 'value))
-      (get-optional args
-                    (errorf "kahua-dbm: no data for key ~s in database ~s"
-                            key class)))))
+         (class (kahua-dbm-class self)))
+    (cond ((find-kahua-instance class 'key k)
+	   => (lambda (obj)
+		(%dbm-s2v self (slot-ref obj 'value))))
+	  (else
+	   (get-optional args
+			 (errorf "kahua-dbm: no data for key ~s in database ~s"
+				 key class))))))
 
 (define-method dbm-exists? ((self <kahua-dbm>) key)
   (next-method)
-  (and-let* ((obj (find-kahua-instance (kahua-dbm-class self)
-                                       (%dbm-k2s self key))))
-    (not (ref obj 'deleted?))))
+  (find-kahua-instance (kahua-dbm-class self)
+		       'key (%dbm-k2s self key)))
 
 (define-method dbm-delete! ((self <kahua-dbm>) key)
   (next-method)
   (and-let* ((obj (find-kahua-instance (kahua-dbm-class self)
-                                       (%dbm-k2s self key))))
-    (set! (ref obj 'deleted?) #t)
+                                       'key (%dbm-k2s self key))))
+    (remove-kahua-instance obj)
     #t))
 
 (define-method dbm-fold ((self <kahua-dbm>) proc seed)
   (next-method)
   (fold (lambda (obj seed)
-          (if (ref obj 'deleted?)
-            seed
-            (proc (%dbm-s2k self (ref obj 'key))
-                  (%dbm-s2v self (ref obj 'value))
-                  seed)))
+	  (proc (%dbm-s2k self (ref obj 'key))
+		(%dbm-s2v self (ref obj 'value))
+		seed))
         seed (make-kahua-collection (kahua-dbm-class self))))
 
 (define-method dbm-db-exists? ((class <kahua-dbm-meta>) name)
@@ -114,5 +105,3 @@
   (errorf "removing kahua-dbm database isn't supported yet"))
 
 (provide "kahua/dbm")
-
-
