29cc6bd1565be30ad9ef15d588ddaba1790b1314
ENDO Yasuyuki <yasuyuki@javaopen.org>
2008-02-10 01:33:20 +0000

    * test/config.scm ("error-document-alist"): 503 (instead of 500)
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/Kahua/trunk@3236 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- test/config.scm
+++ test/config.scm
@@ -36,18 +36,18 @@
        (kahua-logpath "foo.log"))
 
 (test* "config file" "./test.conf"
-       (kahua-config-file))
+       (kahua-config-file))xs
 
-(test* "error-document-alist" '((404 . "_work/404.html") (500 . "_work/500.html"))
+(test* "error-document-alist" '((404 . "_work/404.html") (503 . "_work/503.html"))
        (kahua-error-document-alist))
 
 (test* "error-document 404" "_work/404.html"
        (kahua-error-document 404))
 
-(test* "error-document 500" "_work/500.html"
+(test* "error-document 500" #f
        (kahua-error-document 500))
 
-(test* "error-document 503" #f
+(test* "error-document 503" "_work/503.html"
        (kahua-error-document 503))
 
 (test-section "Initialize with Site Bundle")
