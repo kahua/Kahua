8d4c2898b1cef60ee06ecf6156b2b719af4f39b0
Katsutoshi Itoh <cut-sea@cutsea110@gmail.com>
2008-04-06 12:02:15 +0000

    2008-04-06  Katsutoshi Itoh <cut-sea@kahua.org>
    	* Change: plugins/dsl-simple.scm
    	          change APIs, and add usefull widgets like as calendar date selector.
    
    --
    
    M    plugins/dsl-simple.scm
    M    ChangeLog
    
    
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/Kahua/trunk@3315 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- plugins/dsl-simple.scm
+++ plugins/dsl-simple.scm
@@ -5,15 +5,21 @@
 ;;
 ;; dsl for simple page
 ;;
-
+(use srfi-1)
 (use srfi-13)
+(use srfi-19)
+(use gauche.collection)
+(use util.list)
 (use kahua.elem)
 
 (define-plugin "dsl-simple"
   (version "0.1")
-  (export define-main-entry
+  (export anonpage
+	  page
+	  define-main-entry
 	  define-page
 	  define-main-page
+	  define-anonmain-page
 
 	  selfish
 
@@ -27,13 +33,33 @@
 	  modifytext/
 	  comment/
 	  submit/
-	  post/
+	  button/
 	  confirm/
 ;;	  reset/
 	  dropdown/
 	  multisel/
+
+	  ;; calendar
+	  make-month
+	  make-day
+	  first-day-of-month
+	  next-month
+	  prev-month
+	  days-of-month
+	  date-slices-of-month
+	  date->ymd
+	  ymd->date
+	  calendar/
 	  )
   (depend #f))
+
+;; anonymous page
+(define (anonpage . body)
+  (html/ (head/ (title/ "no title")) (apply body/ body)))
+
+;; page
+(define (page ttl . body)
+  (html/ (head/ (title/ ttl)) (apply body/ body)))
 
 ;; define-entry and set the entry as main-proc
 ;;
@@ -48,16 +74,21 @@
 (define-macro (define-page ent . body)
   (let1 ttl (string-upcase (x->string (car ent)))
     `(define-entry ,ent
-       (html/ (head/ (title ,ttl))
-	      (body/ (h1/ (a/cont/ (@@/ (cont ,@ent)) ,ttl)) ,@body)))))
+       (page ,ttl
+	     (h1/ (a/cont/ (@@/ (cont ,@ent)) ,ttl)) ,@body))))
 
 ;; define simple page as main-proc
 ;;
 (define-macro (define-main-page ent . body)
   (let1 ttl (string-upcase (x->string (car ent)))
     `(define-main-entry ,ent
-       (html/ (head/ (title/ ,ttl))
-	      (body/ (h1/ (a/cont/ (@@/ (cont ,@ent)) ,ttl)) ,@body)))))
+       (page ,ttl
+	     (h1/ (a/cont/ (@@/ (cont ,@ent)) ,ttl)) ,@body))))
+
+(define-macro (define-anonmain-page ent . body)
+  (let1 ttl (string-upcase (x->string (car ent)))
+    `(define-main-entry ,ent
+       (anonpage ,@body))))
 
 ;; self update macro
 ;;
@@ -77,93 +108,82 @@
 
 ;; read line
 ;;
-(define-macro (readln/ name)
-  (let1 n (x->string name)
-    `(label/ ,#`",n :" (input/ (@/ (name ,n))))))
+(define (readln/ var)
+  (label/ #`",var :" (input/ (@/ (name var)))))
 
 ;; read password
 ;;
-(define-macro (readpass/ pass)
-  (let1 p (x->string pass)
-    `(label/ ,#`",p :" (input/ (@/ (type "password") (name ,p))))))
+(define (readpass/ var)
+  (label/ #`",var :" (input/ (@/ (type "password") (name var)))))
 
 ;; file upload
 ;;
-(define-macro (fileref/ file)
-  (let1 f (x->string file)
-    `(label/ ,#`",f :" (input/ (@/ (type "file") (name ,f))))))
+(define (fileref/ var)
+  (label/ #`",var :" (input/ (@/ (type "file") (name var)))))
 
 ;; one checkbox
 ;;
-(define-macro (check/ cbox)
-  (let1 c (x->string cbox)
-    `(label/ (input/ (@/ (type "checkbox") (name ,c))) ,c)))
+(define (check/ var)
+  (label/ (input/ (@/ (type "checkbox") (name var))) var))
 
 ;; checkbox set
 ;;
-(define-macro (check-set/ name lst)
-  `(let ((n ,(x->string name))
-	 (ls (map x->string ,lst)))
-     (label/ #`",n :"
-	     (map/ (lambda (v)
-		     (label/
-		      (input/ (@/ (type "checkbox") (name v)))
-		      v))
-		   ls))))
+(define (check-set/ var lst)
+  (label/ #`",var :"
+	  (map/ (lambda (v)
+		  (label/
+		   (input/ (@/ (type "checkbox") (name v)))
+		   v))
+		lst)))
 
 ;; radio buttons
 ;;
-(define-macro (radio-set/ name lst)
-  `(let* ((nm ,(x->string name))
-	  (ls (map x->string ,lst)))
-     (label/ #`",nm :"
-	     (map/ (lambda (v)
-		     (label/
-		      (input/ (@/ (type "radio") (name nm) (value v)))
-		      v))
-		   ls))))
+(define (radio-set/ var lst)
+  (label/ #`",var :"
+	  (map/ (lambda (v)
+		  (label/
+		   (input/ (@/ (type "radio") (name var) (value v)))
+		   v))
+		lst)))
+
 ;; read text(multi line)
 ;;
-(define-macro (readtext/ name)
-  (let1 n (x->string name)
-    `(label/ ,#`",n :" (textarea/ (@/ (name ,n))))))
+(define (readtext/ var)
+  (label/ #`",var :" (textarea/ (@/ (name var)))))
 
 ;; modify text(multi line)
 ;;
-(define-macro (modifytext/ name text)
-  (let ((n (x->string name))
-	(val text))
-    `(label/ ,#`",n :" (textarea/ (@/ (name ,n)) ,val))))
+(define (modifytext/ var text)
+  (label/ #`",var :" (textarea/ (@/ (name var)) text)))
 
 ;; comment
 ;;
-(define-macro (comment/ name val)
-  (let1 n (x->string name)
-    `(let ()
-       (define (comment-sub/ val)
-	 (form/cont/ (@/ (id ,n))
-	   (@@/ (target ,n)
-		(parts-cont (entry-lambda (:keyword preview save ,name)
-			      (if preview
-				  (comment-sub/ ,name)
-				  (node-set/
-				    (pre/ ,name)
-				    (comment-sub/ ""))))))
-	   (pre/ val)
-	   (textarea/ (@/ (name ,n)) val)
-	   (post/ "preview") (post/ "save")))
-       (comment-sub/ ,val))))
+(define (comment/ var val)
+  (define (comment-sub/ val)
+    (form/cont/ (@/ (id var))
+      (@@/ (target var)
+	   (parts-cont (entry-lambda (:keyword preview save)
+			 (if preview
+			     (comment-sub/ (kahua-context-ref var))
+			     (node-set/
+			       (pre/ (kahua-context-ref var))
+			       (comment-sub/ ""))))))
+      (pre/ val)
+      (textarea/ (@/ (name var)) val)
+      (button/ "preview") (button/ "save")))
+  (comment-sub/ val))
+
+
 
 ;; submit button
 ;;
 (define (submit/)
   (input/ (@/ (type "submit") (name "submit"))))
 
-;; post
+;; button
 ;;
-(define-macro (post/ val)
-  (let1 n (x->string val)
-    `(input/ (@/ (type "submit") (name ,n) (value ,val)))))
+(define (button/ val)
+  (input/ (@/ (type "submit") (name val) (value val))))
 
 ;; submit with confirm
 ;;
@@ -178,27 +198,105 @@
 
 ;; drop down selector
 ;;
-(define-macro (dropdown/ name lst)
-  `(let* ((nm ,(x->string name))
-	  (ls (map x->string ,lst)))
-     (label/ (@/ (style "display: block")) #`",nm :"
-	     (select/
-	      (@/ (name nm))
-	      (map/ (lambda (v)
-		      (option/ (@/ (value v)) v)) ls)))))
+(define (dropdown/ var lst)
+  (label/ #`",var :"
+	  (select/
+	   (@/ (name var))
+	   (map/ (lambda (v)
+		   (option/ (@/ (value v)) v))
+		 lst))))
 
 ;; multi selector like as dropdown
 ;;
-(define-macro (multisel/ name lst)
-  `(let* ((nm ,(x->string name))
-	  (l (min 5 (length ,lst)))
-	  (ls (map x->string ,lst)))
-     (label/ (@/ (style "display: block")) #`",nm :"
-	     (select/
-	      (@/ (name nm) (multiple #t) (size l))
-	      (map/ (lambda (v)
-		      (option/ (@/ (value v)) v)) ls)))))
+(define (multisel/ var lst)
+  (let1 l (min 5 (length lst))
+    (label/ #`",var :"
+	    (select/
+	     (@/ (name var) (multiple #t) (size l))
+	     (map/ (lambda (v)
+		     (option/ (@/ (value v)) v))
+		   lst)))))
 
+(define (value/ var val)
+  (input/ (@/ (type "hidden") (name var) (value val))))
+
+;;-------------------------------------------
+;; calendar
+;;
+(define (make-month m y)
+  (make-date 0 0 0 0 1 m y (date-zone-offset (current-date))))
+
+(define (make-day y m d)
+  (make-date 0 0 0 0 d m y (date-zone-offset (current-date))))
+
+(define (first-day-of-month date)
+  (make-month (date-month date) (date-year date)))
+
+(define (next-month date)
+  (if (= (date-month date) 12)
+      (make-month 1 (+ (date-year date) 1))
+      (make-month (+ (date-month date) 1) (date-year date))))
+
+(define (prev-month date)
+  (if (= (date-month date) 1)
+      (make-month 12 (- (date-year date) 1))
+      (make-month (- (date-month date) 1) (date-year date))))
+
+(define (days-of-month date)
+  (inexact->exact
+   (- (date->modified-julian-day (next-month date))
+      (date->modified-julian-day (first-day-of-month date)))))
+
+(define (date-slices-of-month date)
+  (slices (append (make-list (date-week-day (first-day-of-month date)) #f)
+		  (iota (days-of-month date) 1))
+	  7 #t #f))
+
+(define (date->ymd date) (date->string date "~Y/~m/~d"))
+
+(define (ymd->date ymd) (string->date ymd "~Y/~m/~d"))
+
+(define (calendar/ @id ttl date . selected-date)
+  (let1 selected-date (get-optional selected-date #f)
+    (table/
+     (@/ (id #`",|@id|-tbl"))
+     (thead/
+      (tr/ (td/ (@/ (colspan 3)) ttl)
+	   (td/ (@/ (colspan 4))
+		(value/ @id selected-date))))
+     (tbody/
+      (tr/ (td/ (a/cont/
+		    (@@/ (target #`",|@id|-tbl")
+			 (keep #t)
+			 (fragment #`",|@id|-tbl") ;; for test
+			 (parts-cont
+			  (cut calendar/ @id ttl (prev-month date) selected-date)))
+		  "←"))
+	   (td/ (@/ (colspan 5) (align "center"))
+		#`",(date-year date)/,(date-month date)")
+	   (td/ (a/cont/
+		    (@@/ (target #`",|@id|-tbl")
+			 (keep #t)
+			 (fragment #`",|@id|-tbl") ;; for test
+			 (parts-cont
+			  (cut calendar/ @id ttl (next-month date) selected-date)))
+		  "→"))))
+     (tr/ (map/ td/ "SMTWTFS"))
+     (map/ (lambda (w)
+	     (tr/ (map/ (lambda (d)
+			  (let1 ymd #`",(date-year date)/,(date-month date)/,d"
+			    (td/ (@/ (bgcolor (if (equal? ymd selected-date)
+						  "pink" "white")))
+				 (a/cont/
+				   (@@/ (target #`",|@id|-tbl")
+					(keep #t)
+					(parts-cont
+					 (cut calendar/ @id ttl date ymd)))
+				   (@/ (style "text-decoration: none"))
+
+				   d))))
+			w)))
+	   (date-slices-of-month date)))))
 
 
 ;;; Local Variables:
