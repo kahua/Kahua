164ceeb6d040ce22d615d46f67702b536f627d3d
ENDO Yasuyuki <yasuyuki@javaopen.org>
2008-10-17 08:19:43 +0000

    add
    git-svn-id: svn+ssh://svn.kahua.org/var/local/repos/kahua/Kahua/trunk@3430 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

---
+++ contrib/staging/playback-httplog.scm
@@ -0,0 +1,24 @@
+#!/usr/local/bin/gosh
+
+(use rfc.http)
+
+(define (help args)
+  (print #`"Usage: ,(car args) host logfile"))
+
+(define (print-status host uri)
+  (receive (status header body)
+      (http-get host uri)
+    (format #`"~d ~a" status uri)))
+
+(define (main args)
+  (cond [(< (length+ args) 3) (help args)]
+	[else
+	 (let [(host (cadr args))
+	       (logfile (caddr args))]
+	   (call-with-input-file logfile
+	     (lambda (in)
+	       (for-each
+		(lambda (line)
+		  (let ((words (string-split line " ")))
+		    (print-status host (list-ref words 6))))
+		(port->string-list in)))))]))
